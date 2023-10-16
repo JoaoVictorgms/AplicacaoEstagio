bf0577641a289e8c469a88985fc3293ecd4abbec
joao victor gomes <joao-iccz@hotmail.com>
2023-10-16 10:35:48 -0300

    factory

--- src/main/java/factory/Factory.java
+++ src/main/java/factory/Factory.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package factory;
 
-/**
- *
- * @author jvictor
- */
+import com.aplicaoestagio.aplicacaoestagio.entidades.Aluno;
+import com.aplicaoestagio.aplicacaoestagio.entidades.Empresa;
+import com.aplicaoestagio.aplicacaoestagio.entidades.Estagio;
+import com.aplicaoestagio.aplicacaoestagio.entidades.Orientador;
+
+import java.time.LocalDate;
+
 public class Factory {
-    
+
+    public static Aluno criarAluno() {
+        return new Aluno();
+    }
+
+    public static Empresa criarEmpresa() {
+        return new Empresa();
+    }
+
+    public static Estagio criarEstagio(LocalDate inicio, LocalDate fim, int cargaHoraria, String status) {
+        return new Estagio(inicio, fim, cargaHoraria, status);
+    }
+
+    public static Orientador criarOrientador() {
+        return new Orientador();
+    }
 }
